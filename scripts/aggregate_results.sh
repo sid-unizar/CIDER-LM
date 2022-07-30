a3e4c0b7089a14a2b536668928349011e32b9acb
Javier Vela <javier.vela00@gmail.com>
2022-07-30 10:40:07 +0200

    Update script for results

--- scripts/aggregate_results.sh
+++ scripts/aggregate_results.sh
@@ -1,9 +1,22 @@
 #!/bin/zsh
 
+TRACKDIR="1-3-val_1"
+ALIGNMENT_NUMER="363" # 362 + 1
+
+incorrect=0
 # Move
-for f in ciderlm/results/results_*; do
-	cp "$f/multifarm_all-v2/aggregated/Main/aggregatedPerformance.csv" "results-aggregated/results_`echo $f | cut -d ' ' -f 1 --complement `.csv"
+for f in ciderlm/results/*; do
+	if [ `ls $f/$TRACKDIR/ | wc -l` -ne $ALIGNMENT_NUMER ]; then
+		echo "Wrong number of final alignments!!!!!!!! in $f"
+		incorrect=1
+	else
+		cp "$f/$TRACKDIR/aggregated/Main/aggregatedPerformance.csv" "results-aggregated/results_`echo $f | cut -d ' ' -f 1 --complement `.csv"
+	fi
 done
+
+if [ $incorrect -eq 1 ]; then
+	#exit 1
+fi
 
 # Get header
 for f in results-aggregated/results_*; do
