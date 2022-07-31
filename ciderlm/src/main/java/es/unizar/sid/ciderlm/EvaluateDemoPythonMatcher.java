8358491343346981355cf95c78f3343ec89eb1c8
Javier Vela <javier.vela00@gmail.com>
2022-07-31 11:16:57 +0200

    Add local track 1-3-val for matcher evaluation

--- ciderlm/src/main/java/es/unizar/sid/ciderlm/EvaluateDemoPythonMatcher.java
+++ ciderlm/src/main/java/es/unizar/sid/ciderlm/EvaluateDemoPythonMatcher.java
@@ -1,12 +1,14 @@
 package es.unizar.sid.ciderlm;
 
 import de.uni_mannheim.informatik.dws.melt.matching_data.TrackRepository;
+import de.uni_mannheim.informatik.dws.melt.matching_data.LocalTrack;
 import de.uni_mannheim.informatik.dws.melt.matching_eval.ExecutionResult;
 import de.uni_mannheim.informatik.dws.melt.matching_eval.ExecutionResultSet;
 import de.uni_mannheim.informatik.dws.melt.matching_eval.Executor;
 import de.uni_mannheim.informatik.dws.melt.matching_eval.evaluator.EvaluatorCSV;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.io.File; // Import the File class
 
 /**
  * Please make sure that you have Python and the required dependencies
@@ -20,11 +22,15 @@ public class EvaluateDemoPythonMatcher {
         // in python environment rdflib is required.
         // do not forget to remove the eval dependency in pom.xml when building the
         // seals package (of course you have to comment this whole class too).
-        
+
+        ExecutionResultSet result = Executor.run(new LocalTrack("1-3-val", "1",
+                "/home/javier/oaei_track_cache/oaei.webdatacommons.org/multifarm/1-3-val/"), new Main());
+
         // TODO parameter?\
-        
-        ExecutionResultSet result = Executor.run(TrackRepository.Multifarm.ALL_IN_ONE_TRACK, new Main());
-        // TODO ExecutionResultSet result = Executor.run(TrackRepository.Conference.V1, new Main()); // Different track
+        // ExecutionResultSet result =
+        // Executor.run(TrackRepository.Multifarm.ALL_IN_ONE_TRACK, new Main());
+        // TODO ExecutionResultSet result = Executor.run(TrackRepository.Conference.V1,
+        // new Main()); // Different track
         ExecutionResult r = result.iterator().next();
         LOGGER.info("Python matcher run returned {} correspondences.", r.getSystemAlignment().size());
         EvaluatorCSV evaluatorCSV = new EvaluatorCSV(result);
