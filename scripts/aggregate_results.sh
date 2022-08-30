53ed9b4d7324ea50c61a8b88488474f8e9e629a5
Javier Vela <javier.vela00@gmail.com>
2022-08-30 17:49:17 +0200

    Update results

--- scripts/aggregate_results.sh
+++ scripts/aggregate_results.sh
@@ -6,25 +6,25 @@ ALIGNMENT_NUMER="363" # 362 + 1
 incorrect=0
 # Move
 for f in ciderlm/results/*; do
-	if [ `ls $f/$TRACKDIR/ | wc -l` -ne $ALIGNMENT_NUMER ]; then
-		echo "Wrong number of final alignments!!!!!!!! in $f"
-		incorrect=1
-	else
-		cp "$f/$TRACKDIR/aggregated/Main/aggregatedPerformance.csv" "results-aggregated/results_`echo $f | cut -d ' ' -f 1 --complement `.csv"
-	fi
+	# if [ `ls $f/$TRACKDIR/ | wc -l` -ne $ALIGNMENT_NUMER ]; then
+	# 	echo "Wrong number of final alignments!!!!!!!! in $f"
+	# 	incorrect=1
+	# else
+	cp "$f/$TRACKDIR/aggregated/Main/aggregatedPerformance.csv" "results-aggregated/results_$(echo $f | cut -d ' ' -f 1 --complement).csv"
+	# fi
 done
 
-if [ $incorrect -eq 1 ]; then
-	#exit 1
-fi
+# if [ $incorrect -eq 1 ]; then
+# 	#exit 1
+# fi
 
 # Get header
 for f in results-aggregated/results_*; do
-	echo `cat $f | head -1`
+	echo $(cat $f | head -1)
 	break
-done > results-aggregated/aggregated-total.csv
+done >results-aggregated/aggregated-total.csv
 
-# Get results with version as header 
+# Get results with version as header
 for f in results-aggregated/results_*; do
-	echo `cat $f | head -2 | tail -1` | sed --expression="s/ALL/`echo $f | cut -d '_' -f 1 --complement | rev | cut -d '.' -f 1 --complement | rev`/g"
-done >> results-aggregated/aggregated-total.csv
+	echo $(cat $f | head -2 | tail -1) | sed --expression="s/ALL/$(echo $f | cut -d '_' -f 1 --complement | rev | cut -d '.' -f 1 --complement | rev)/g"
+done >>results-aggregated/aggregated-total.csv
